50dae8ec6e1ffe7f43ffcf39cb17109dd525c22c
dino konstantopoulos <dinorows@users.noreply.github.com>
2021-03-14 18:31:50 -0400

    Update twtr.py

--- twtr.py
+++ twtr.py
@@ -21,8 +21,8 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 # mongo
-#mongo_client = MongoClient('mongodb://localhost:27017/')
-mongo_client = MongoClient("mongodb+srv://admin:admin@tweets.8ugzv.mongodb.net/tweets?retryWrites=true&w=majority")
+mongo_client = MongoClient('mongodb://localhost:27017/')
+#mongo_client = MongoClient("mongodb+srv://<user>:<password>@<collection>.<code>.mongodb.net/<db>?retryWrites=true&w=majority")
 
 app = Flask(__name__)
 CORS(app)
